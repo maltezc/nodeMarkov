5c7582f3041af54a8f04bc7f26199d8112cec747
Christopher Maltez <cflux.maltez@live.com>
2023-01-03 15:30:20 -0800

    working on getText()

--- markov.js
+++ markov.js
@@ -1,8 +1,6 @@
 /** Textual markov chain generator. */
 
-
 class MarkovMachine {
-
   /** Build markov machine; read in text.*/
 
   constructor(text) {
@@ -15,7 +13,7 @@ class MarkovMachine {
   /** Get markov chain: returns Map of Markov chains.
    *
    *  For text of "The cat in the hat.", chains will be:
-   * 
+   *
    *  {
    *   "The": ["cat"],
    *   "cat": ["in"],
@@ -23,22 +21,51 @@ class MarkovMachine {
    *   "the": ["hat."],
    *   "hat.": [null],
    *  }
-   * 
+   *
    * */
 
   getChains() {
-    // TODO: implement this!
-  }
+    // // TODO: implement this!
+    // initialize object
+    let map = {};
 
+    // for loop word in words
+    for (let index = 0; index < this.words.length; index++) {
+      const word = this.words[index];
+
+      // if word + 1 == undefined,
+      if (this.words[index + 1] !== undefined) {
+        // set key as word.
+        // set value as word index + 1
+        map[word] = [this.words[index + 1]];
+        // push word as key and null as word + 1
+      } else {
+        map[word] = [null];
+      }
+    }
+    return map;
+  }
 
   /** Return random text from chains, starting at the first word and continuing
    *  until it hits a null choice. */
 
   getText() {
     // TODO: implement this!
-
+    const firstWord = this.words[0]
     // - start at the first word in the input text
+    let sentenceList = [firstWord];
+
+    while ()
     // - find a random word from the following-words of that
+      const randomIndexInRow = Math.floor(Math.random() * this.chains[firstWord].length);
+      const randomWord = this.chains[firstWord][randomIndexInRow];
+      sentenceList.push(randomWord)
+
+    }
+
     // - repeat until reaching the terminal null
   }
 }
+
+const catInHatMachine = new MarkovMachine("the cat in the hat");
+console.log(catInHatMachine.chains);
